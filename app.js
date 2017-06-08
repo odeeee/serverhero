5ada517b024ca02733319cd666d232d11d3fcce9
odeeee <hyvonenotto@gmail.com>
2017-06-08 14:13:48 +0300

    torstai86

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var mysql = require('mysql');
 
 var data = fs.readFileSync('naytot.json');
 var naytot = JSON.parse(data);
@@ -20,6 +21,34 @@ app.listen(port , function(){
 	console.log('server listening');
 });
 
+var connection = mysql.createConnection({
+  connectionLimit : 100,
+  host     : 'www.db4free.net',
+  port     :  3306,
+  user     : 'porkkana',
+  password : 'orava1337',
+  database : 'ovensausage'
+});
+
+connection.connect(function(err) {
+  if(err){
+    console.log('Error connecting to Db ' + err.stack);
+
+    return;
+  }else{
+   console.log('Connection established');
+	}
+});
+
+connection.query('SELECT * from ostable', function(err, rows, fields) {
+  if (!err)
+    console.log('The solution is: ', rows);
+  else
+    console.log('Error while performing Query. ');
+});
+
+//connection.end();
+
 app.get('/info', sendStuff);
 
 function sendStuff(request , response) {
@@ -27,15 +56,24 @@ function sendStuff(request , response) {
 	console.log('sendStuff loaded');
 }
 
-app.get('/add/:beacon/:ryhma', addInfo);
+app.get('/add/1/:beacon/:ryhma/:saa', addInfo);
 
 function addInfo(request , response){
-		console.log('ADDINFO')
+		console.log('addInfo')
 	var data = request.params;
-	var beacon = data.beacon;
+	var beacon1 = data.beacon;
+	var beacon = beacon1.toString();
 	var ryhma = data.ryhma;
+	var saa = data.saa;
 
 	naytot[beacon] = ryhma;
+
+	var post  = {Major: beacon, Ryhma: ryhma, Saa: saa,Viesti:" " };
+
+  	connection.query('UPDATE ostable SET ? Where Major = ?', [post , beacon] , function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  	});
 
 	var tieto = JSON.stringify(naytot , null ,2);
 	fs.writeFile('naytot.json' , tieto , finished);
@@ -46,11 +84,50 @@ function addInfo(request , response){
 	var reply = {
 		beacon: beacon,
 		ryhma: ryhma,
+		saa: saa,
 		status:"all done"
 	}
 
 	response.send(reply);
 	}
+}
+
+app.get('/add/2/:beacon/:ryhma/:saa/:viesti', addInfoV);
+
+function addInfoV(request , response){
+	console.log('addInfoV');
+	var data = request.params;
+	var beacon1 = data.beacon;
+	var beacon = beacon1.toString();
+	var ryhma = data.ryhma;
+	var saa = data.saa;
+	var viesti = data.viesti;
+
+	naytot[beacon] = ryhma;
+
+	var post  = {Major: beacon, Ryhma: ryhma, Saa: saa,Viesti: viesti};
+
+  	connection.query('UPDATE ostable SET ? Where Major = ?', [post , beacon] , function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  	});
+
+	var tieto = JSON.stringify(naytot , null ,2);
+	fs.writeFile('naytot.json' , tieto , finished);
+
+	function finished(err){
+		console.log('all set');
+	
+	var reply = {
+		beacon: beacon,
+		ryhma: ryhma,
+		saa: saa,
+		status:"all done"
+	}
+
+	response.send(reply);
+	}
+}
 
 app.get('/beacon/:major', openSite);
 
@@ -82,5 +159,4 @@ function openSite(request , response){
     return objects;
 	}
 	
-}
 }
