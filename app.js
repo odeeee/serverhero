f84f99ac34dd1d236a1f62093c257a44e533a6ac
odeeee <hyvonenotto@gmail.com>
2017-07-07 12:45:24 +0300

    virhe korjattu

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 var fs = require('fs');
-var mysql = require('mysql');
-
+var mysql = require('mysql')
 var data = fs.readFileSync('naytot.json');
 var naytot = JSON.parse(data);
-
 var express = require('express');
 var app =express();
 //app.use(express.static(__dirname + '/public'));
 
 var path = require('path'); //Use the path to tell where find the .ejs files
+
 // view engine setup
 app.set('views', path.join(__dirname, '/views')); // here the .ejs files is in views folders
 app.set('view engine', 'ejs'); //tell the template engine
@@ -50,23 +49,24 @@ var connection = mysql.createConnection({
   });
   	}); 
 */
+//Yhdistää databaseen
 function connDb(){
-console.log('Connecting to database...');
-connection.connect(function(err) {
-  if(err){
-    console.log('Error connecting to Db ' + err.stack);
-    return;
-  }else{
-   	console.log('Connection established');
-   	databTulostus();
-  }
-});
+  console.log('Connecting to database...');
+  connection.connect(function(err) {
+    if(err){
+      console.log('Error connecting to Db ' + err.stack);
+      return;
+    }else{
+   	  console.log('Connection established');
+   	  databTulostus();
+    }
+  });
 }
 
-//Yllä pitää yhteyttä
+//Yllä pitää yhteyttä Databaseen 
 setInterval(function () {
-    connection.query('SELECT 1');
-	}, 5000);
+  connection.query('SELECT 1');
+}, 5000);
 
 function databTulostus(){
 	connection.query('SELECT * from vstable2', function(err, rows, fields) {
@@ -85,6 +85,20 @@ function databTulostus(){
 }
 //connection.end();
 
+//Tällä haetaan arvoja key:llä JSON tiedostosta
+function getValues(obj, key) {
+      var objects = [];
+      for (var i in obj) {
+          if (!obj.hasOwnProperty(i)) continue;
+          if (typeof obj[i] == 'object') {
+              objects = objects.concat(getValues(obj[i], key));
+          } else if (i == key) {
+              objects.push(obj[i]);
+          }
+      }
+    return objects;
+  }
+
 app.get('/info', sendStuff);
 
 function sendStuff(request , response) {
@@ -101,147 +115,117 @@ function addInfo(request , response){
   var data = request.params;
   var beacon1 = data.beacon;
 
-  function getValues(obj, key) {
-      var objects = [];
-      for (var i in obj) {
-          if (!obj.hasOwnProperty(i)) continue;
-          if (typeof obj[i] == 'object') {
-              objects = objects.concat(getValues(obj[i], key));
-          } else if (i == key) {
-              objects.push(obj[i]);
-          }
-      }
-    return objects;
-  }
-
   if (ryhmaTunnus != "null" && beacon1 != "null") {
     console.log("add1: ryhmaTunnus beaconilla varattu");
   }else{
 	
-	var beacon = beacon1.toString();
-	var ryhma = data.ryhma;
-	var saa = data.saa;
-	naytot[beacon] = ryhma;
-	var viesti = " ";
+	  var beacon = beacon1.toString();
+	  var ryhma = data.ryhma;
+	  var saa = data.saa;
+	  naytot[beacon] = ryhma;
+	  var viesti = " ";
   
-	connection.query('SELECT Viesti FROM vstable2 Where Ryhma = ?', [ryhma] , function (err, result) {
-    if (err) {
-    	//throw err;
-      console.err;
-      viesti = "Ryhmälle ei ole viestiä";
-      console.log('Ryhmällä ei viestiä');
-      updateDb();
-    }else{
-      if(result!="")
-      {
-    	console.log("Viesti haettu " + result[0].Viesti);
-    	setViesti(result);
-      }else{
-        console.log("Ei viestiä");
-        viesti = " ";
+	  connection.query('SELECT Viesti FROM vstable2 Where Ryhma = ?', [ryhma] , function (err, result) {
+      if (err) {
+    	 //throw err;
+        console.err;
+        viesti = "Ryhmälle ei ole viestiä";
+        console.log('Ryhmällä ei viestiä');
         updateDb();
-      }
-	}
-  	});
+      }else{
+        if(result!=""){
+    	    console.log("Viesti haettu " + result[0].Viesti);
+    	    setViesti(result);
+        }else{
+          console.log("Ei viestiä");
+          viesti = " ";
+          updateDb();
+        }
+	    }
+    });
 
   	function setViesti(result) {
   		viesti = result[0].Viesti;
   		console.log("viesti: " + viesti);
   		updateDb();
-	}
+	  }
 
-	function updateDb(){
-		var post  = {Major: beacon, Ryhma: ryhma, Saa: saa,Viesti: viesti };
+	  function updateDb(){
+		  var post  = {Major: beacon, Ryhma: ryhma, Saa: saa,Viesti: viesti };
   		connection.query('UPDATE ostable SET ? Where Major = ?', [post , beacon] , function (err, result) {
     	if (err) throw err;
     	console.log("1 record inserted");
     	databTulostus();
-  	});
+  	  });
+    }
 
-	var tieto = JSON.stringify(naytot , null ,2);
-	fs.writeFile('naytot.json' , tieto , finished);
+	  var tieto = JSON.stringify(naytot , null ,2);
+	  fs.writeFile('naytot.json' , tieto , finished);
 
-	function finished(err){
-		console.log('all set');
-	
-	var reply = {
-		beacon: beacon,
-		ryhma: ryhma,
-		saa: saa,
-		status:"all done"
-	}
-	response.send(reply);
-	}
+	  function finished(err){
+	    console.log('all set');
+	 
+      var reply = {
+		  beacon: beacon,
+		  ryhma: ryhma,
+		  saa: saa,
+		  status:"all done"
+	    }
+	  response.send(reply);
+	  }
+  }
 }
-}
-}
+
 
 app.get('/add/2/:beacon/:ryhma/:saa/:viesti', addInfoV);
 
 function addInfoV(request , response){
 	console.log('addInfoV');
 
-var ryhmaTunnus = getValues(naytot,key);
-var data = request.params;
-var beacon1 = data.beacon;
-
-  function getValues(obj, key) {
-      var objects = [];
-      for (var i in obj) {
-          if (!obj.hasOwnProperty(i)) continue;
-          if (typeof obj[i] == 'object') {
-              objects = objects.concat(getValues(obj[i], key));
-          } else if (i == key) {
-              objects.push(obj[i]);
-          }
-      }
-    return objects;
-  }
+  var ryhmaTunnus = getValues(naytot,key);
+  var data = request.params;
+  var beacon1 = data.beacon;
 
   if (ryhmaTunnus != "null" && beacon1 != "null") {
     console.log("add2: ryhmaTunnus beaconilla varattu");
   }else{
-	
-	var beacon = beacon1.toString();
-	var ryhma = data.ryhma;
-	var saa = data.saa;
-	var viesti = data.viesti;
+	  var beacon = beacon1.toString();
+	  var ryhma = data.ryhma;
+	  var saa = data.saa;
+	  var viesti = data.viesti;
+	  naytot[beacon] = ryhma;
+	  var ryhm = {Ryhma: ryhma};
+	  var viest  = {Ryhma: ryhma ,Viesti: viesti};
 
-	naytot[beacon] = ryhma;
-
-	var ryhm = {Ryhma: ryhma};
-	var viest  = {Ryhma: ryhma ,Viesti: viesti};
-
-	connection.query('INSERT INTO vstable2 SET ? ON DUPLICATE KEY UPDATE Viesti = ? ', [viest, viesti] , function (err, result) {
-    	if (err) throw err;
+	  connection.query('INSERT INTO vstable2 SET ? ON DUPLICATE KEY UPDATE Viesti = ? ', [viest, viesti] , function (err, result) {
+     	if (err) throw err;
     	console.log("viesti record inserted");
-  	});
+    });
 
-	var post  = {Major: beacon, Ryhma: ryhma, Saa: saa,Viesti: viesti};
+	  var post  = {Major: beacon, Ryhma: ryhma, Saa: saa,Viesti: viesti};
 
-  	connection.query('UPDATE ostable SET ? Where Major = ?', [post , beacon] , function (err, result) {
+    connection.query('UPDATE ostable SET ? Where Major = ?', [post , beacon] , function (err, result) {
     	if (err) throw err;
-    	console.log("1 record inserted");
-    	databTulostus();
-  	});
+    	 console.log("1 record inserted");
+    	 databTulostus();
+    });
 
-	var tieto = JSON.stringify(naytot , null ,2);
-	fs.writeFile('naytot.json' , tieto , finished);
+	  var tieto = JSON.stringify(naytot , null ,2);
+	  fs.writeFile('naytot.json' , tieto , finished);
 
-	function finished(err){
-		console.log('all set');
+	  function finished(err){
+		  console.log('all set');
 	
-	var reply = {
-		beacon: beacon,
-		ryhma: ryhma,
-		saa: saa,
-		viesti: viesti,
-		status:"all done"
-	}
-
-	response.send(reply);
-	}
-}
+	    var reply = {
+		    beacon: beacon,
+		    ryhma: ryhma,
+		    saa: saa,
+		    viesti: viesti,
+		    status:"all done"
+	    }
+	    response.send(reply);
+	  }
+  } 
 }
 
 app.get('/viesti/:ryhma/:msg/:user', tallennaViesti);
@@ -252,13 +236,12 @@ function tallennaViesti(request , response){
   var ryhma = data.ryhma;
   var msg = data.msg;
   var user = data.user;
-
   var viest  = {Ryhma: ryhma ,Viesti: msg};
 
   connection.query('INSERT INTO vstable2 SET ? ON DUPLICATE KEY UPDATE Viesti = ? ', [viest, msg] , function (err, result) {
       if (err) throw err;
       console.log("viesti record inserted");
-    });
+  });
 
   var reply = {
     user: user,
@@ -266,7 +249,6 @@ function tallennaViesti(request , response){
     viesti: msg,
     status:"all done"
   }
-
   response.send(reply);
 }
 
@@ -294,35 +276,34 @@ function openSite(request , response){
     		console.log("Data haettu " + result[0]);
     		seTup(result);
 		  }
-  	});
+  });
 	
-  	function seTup(result){
-  		saa = result[0].Saa;
-  		ryhma = result[0].Ryhma;
-  		console.log("Saa , Ryhma: " + saa + " " + ryhma);
-  		haeViesti(ryhma);
-  	}
+  function seTup(result){
+  	saa = result[0].Saa;
+  	ryhma = result[0].Ryhma;
+  	console.log("Saa , Ryhma: " + saa + " " + ryhma);
+  	haeViesti(ryhma);
+  }
 
-  	function haeViesti(ryhma){
-  		console.log(ryhma)
-  		connection.query('SELECT * FROM vstable2 Where Ryhma = ?', [ryhma] , function (err, result) {
-    		if (err) {
-    			throw err;
-    		}else{
-    			console.log("Viesti haettu " + result[0].Viesti);
-    			seTupViesti(result);
-			}
+  function haeViesti(ryhma){
+  	console.log(ryhma)
+  	connection.query('SELECT * FROM vstable2 Where Ryhma = ?', [ryhma] , function (err, result) {
+    	if (err) {
+    		throw err;
+    	}else{
+  			console.log("Viesti haettu " + result[0].Viesti);
+  			seTupViesti(result);
+      }
   	});	
-  	
+  }
 
-  	function seTupViesti(result){
-  		viesti = result[0].Viesti;
-  		console.log("viesti: " + viesti);
-  		asetaSaa();
-  	}
+  function seTupViesti(result){
+		viesti = result[0].Viesti;
+  	console.log("viesti: " + viesti);
+  	asetaSaa();
+  }
 
-    function asetaSaa(){
-
+  function asetaSaa(){
     var url = "http://api.openweathermap.org/data/2.5/weather?q="+saa+"&units=metric&appid=5547c86c9c193df9f6cfb0724ff6994e";
     var http = require('http');
 
@@ -340,49 +321,50 @@ function openSite(request , response){
         asetaRuoka();
       });
     }).on('error', function(e){
-      console.log("Got an error: ", e);
+        console.log("Got an error: ", e);
       });
-    }
+  }
 
-    function asetaRuoka(){
+  function asetaRuoka(){
     var url = "http://www.amica.fi/modules/json/json/Index?costNumber=0235&language=fi";
     var http = require('http');
 
     http.get(url, function(res){
-    var body = '';
-    res.on('data', function(chunk){
+      var body = '';
+      res.on('data', function(chunk){
         body += chunk;
-    });
+      });
 
-    res.on('end', function(){
-      var ruokaLista = JSON.parse(body);
-      ruoka = ruokaLista.MenusForDays;
-      if(typeof ruoka !== 'undefined' && ruoka.length > 0){
-        console.log("Ruokana tänään: ",ruoka);
-        vastaa();
-      }else{
-        ruoka = "Ei ruokaa tänään"
-        console.log("Ruokana tänään: ",ruoka);
-        vastaa();
-      }
-    });
+      res.on('end', function(){
+        var ruokaLista = JSON.parse(body);
+        ruoka = ruokaLista.MenusForDays;
+        if(typeof ruoka !== 'undefined' && ruoka.length > 0){
+          console.log("Ruokana tänään: ",ruoka);
+          vastaa();
+        }else{
+          ruoka = "Ei ruokaa tänään"
+          console.log("Ruokana tänään: ",ruoka);
+          vastaa();
+        }
+      });
     }).on('error', function(e){
-      console.log("Got an error: ", e);
+        console.log("Got an error: ", e);
     });
+  }
 
-    }
-
-  	function vastaa(){
-	if (ryhma != "null") {
-		response.render("index", { 
-			ryhma: ryhma,
-			saa: saa,
-			viesti: viesti,
-      keli: keli,
-      ruoka: ruoka});
-		console.log("Vastattu");
-	}else{
-		console.log("ryhma null");
-		response.sendFile(__dirname + '/public/default.html');
-	}		
+  function vastaa(){
+	  if (ryhma != "null") {
+		  response.render("index", { 
+			 ryhma: ryhma,
+			 saa: saa,
+			 viesti: viesti,
+        keli: keli,
+        ruoka: ruoka
+      });
+		  console.log("Vastattu");
+	  }else{
+		  console.log("ryhma null");
+		  response.sendFile(__dirname + '/public/default.html');
+	  }		
+  }
 }
